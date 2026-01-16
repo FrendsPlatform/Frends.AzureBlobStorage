1ae19dcd799ebcd5346439022d121e8424b5186c
Mateusz Noga-Wojtania <mateusz.noga.wojtania@gmail.com>
2026-01-16 07:18:57 +0100

    fix tests for listblobsInContainer

--- Frends.AzureBlobStorage.ListBlobsInContainer/Frends.AzureBlobStorage.ListBlobsInContainer.Tests/OAuthUnitTests.cs
+++ Frends.AzureBlobStorage.ListBlobsInContainer/Frends.AzureBlobStorage.ListBlobsInContainer.Tests/OAuthUnitTests.cs
@@ -17,7 +17,7 @@ public class OAuthUnitTests
     private readonly string _appID = Environment.GetEnvironmentVariable("Frends_AzureBlobStorage_AppID");
     private readonly string _tenantID = Environment.GetEnvironmentVariable("Frends_AzureBlobStorage_TenantID");
     private readonly string _clientSecret = Environment.GetEnvironmentVariable("Frends_AzureBlobStorage_ClientSecret");
-    private readonly string _storageaccount = "frendstaskstestcontainer";
+    private readonly string _storageaccount = Environment.GetEnvironmentVariable("Frends_AzureBlobStorage_StorageAccount");
 
     [TestInitialize]
     public async Task Init()
