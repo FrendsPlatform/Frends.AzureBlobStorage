448e347679f9ddc30c732e20aeca1dca2e61d501
MichalFrends1 <michal.nowakowski@frends.com>
2026-05-04 21:05:06 +0200

    fixs tests, move DeleteBlobError to new file, update chagelog and migartion file

---
+++ Frends.AzureBlobStorage.DeleteBlob/Frends.AzureBlobStorage.DeleteBlob.Tests/ErrorHandlerTest.cs
@@ -0,0 +1,48 @@
+﻿using Frends.AzureBlobStorage.DeleteBlob.Definitions;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Frends.AzureBlobStorage.DeleteBlob.Tests;
+
+[TestFixture]
+public class ErrorHandlerTest
+{
+    private const string CustomErrorMessage = "CustomErrorMessage";
+
+    [Test]
+    public void Should_Throw_Error_When_ThrowErrorOnFailure_Is_True()
+    {
+        var ex = Assert.ThrowsAsync<Exception>(() =>
+            AzureBlobStorage.DeleteBlob(DefaultInput(), DefaultConnection(), DefaultOptions(), CancellationToken.None));
+        Assert.That(ex, Is.Not.Null);
+    }
+
+    [Test]
+    public async Task Should_Return_Failed_Result_When_ThrowErrorOnFailure_Is_False()
+    {
+        var options = DefaultOptions();
+        options.ThrowErrorOnFailure = false;
+        var result = await AzureBlobStorage.DeleteBlob(DefaultInput(), DefaultConnection(), options, CancellationToken.None);
+        Assert.That(result.Success, Is.False);
+    }
+
+    [Test]
+    public void Should_Use_Custom_ErrorMessageOnFailure()
+    {
+        var options = DefaultOptions();
+        options.ErrorMessageOnFailure = CustomErrorMessage;
+        var ex = Assert.ThrowsAsync<Exception>(() =>
+            AzureBlobStorage.DeleteBlob(DefaultInput(), DefaultConnection(), options, CancellationToken.None));
+        Assert.That(ex, Is.Not.Null);
+        Assert.That(ex.Message, Contains.Substring(CustomErrorMessage));
+    }
+
+    private static Input DefaultInput() => new();
+
+    private static Connection DefaultConnection() => new();
+
+    private static Options DefaultOptions() =>
+        new() { ThrowErrorOnFailure = true, ErrorMessageOnFailure = string.Empty, };
+}
